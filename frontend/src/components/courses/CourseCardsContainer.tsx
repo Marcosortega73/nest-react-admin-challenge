44219ea56445b5b95762ffb7e2ee893d500555e0
Marcos Ortega <marcosortega73@hotmail.com>
2025-08-10 13:47:08 -0300

    feat: container cards responsive

--- frontend/src/components/courses/CourseCardsContainer.tsx
+++ frontend/src/components/courses/CourseCardsContainer.tsx
@@ -2,7 +2,7 @@ import CardItemCourse from './cardItem/CardItemCourse';
 
 export const CourseCardsContainer = () => {
   return (
-    <div className="flex flex-wrap gap-7 w-full">
+    <div className="flex flex-wrap justify-evenly items-center gap-5 md:gap-7 xs:gap-2 sm:gap-3  w-full">
       {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(item => (
         <CardItemCourse key={item} />
       ))}
